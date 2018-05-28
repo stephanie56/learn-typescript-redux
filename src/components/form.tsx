3dc74d4acf7d89ead57af2d07d89e41644713736
Stephanie Zeng <stephaniezeng521@gmail.com>
2018-05-28 15:32:53 -0400

    Extract TextField component & add types

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,18 +1,33 @@
 import * as React from 'react';
 import { rootRef } from '../firebaseconfig/';
 
-const handleOnClick = () => {
-  rootRef.set({
+const handleOnClick = (e) => {
+  e.preventDefault();
+  rootRef.push({
     username: 'name',
     email: 'email',
     profile_picture : 'imageUrl'
   });
 };
 
+interface InputProps {
+  name: string;
+  title: string;
+}
+
+const TextField: React.StatelessComponent<InputProps> = ({name, title}) => (
+  <div>
+    <label htmlFor={name}>{title}</label>
+    <input name={name} type="text" value=""/>
+  </div>
+);
+
 export const Form = () => (
-  <form>
-    <label htmlFor="train-name">Bus Name</label>
-    <input name="train-name" type="text" value=""/>
-    <input type="submit" onClick={handleOnClick} value="submit" />
+  <form onSubmit={(e) => handleOnClick(e)}>
+    <TextField name="bus-name" title="Bus Name"/>
+    <TextField name="stop-number" title="Stop #"/>
+    <TextField name="frequency" title="Frequency"/>
+    <TextField name="next-arrival" title="Next Arrival"/>
+    <input type="submit" value="submit" />
   </form>
 );
